7f9c720f5fdc890786267dd66f0aaad5417c0d89
Navish Sharma <navishsharma2004@gmail.com>
2025-07-03 09:00:03 +0000

    Modified the create controller to use the user model to create a new user

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -5,4 +5,13 @@ class Create extends Controller {
     public function index() {		
 	    $this->view('create/index');
     }
+
+    public function verify(){
+      $username = $_REQUEST['username'];
+      $password = $_REQUEST['password'];
+      $confirm_password = $_REQUEST['confirm-password'];
+
+      $user = $this->model('User');
+      $user->create($username, $password, $confirm_password);
+    }
 }
