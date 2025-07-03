831e28bbb8dcd25eb77b2242951d474c2417a0d3
Navish Sharma <navishsharma2004@gmail.com>
2025-07-03 06:43:13 +0000

    fixed a minor syntax error

--- app/models/User.php
+++ app/models/User.php
@@ -30,7 +30,7 @@ class User {
         $statement->execute();
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
 		
-		if (password_verify($password, $rows['password'])) {
+		if (password_verify($password, $rows['pass'])) {
 			$_SESSION['auth'] = 1;
 			$_SESSION['username'] = ucwords($username);
 			unset($_SESSION['failedAuth']);
